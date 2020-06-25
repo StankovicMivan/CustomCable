7fcc6145836cf453fc368b6b4f94e6ce59bd5c24
Helena Hip <helenahip@gmail.com>
2020-06-25 22:17:24 +0200

    order testing commit

--- src/app/order-preview/order-preview.component.ts
+++ src/app/order-preview/order-preview.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { OrderMod } from '../mailtemplates/orderMod';
 import { Router, ActivatedRoute } from '@angular/router';
 import { stringify } from 'querystring';
+import { OrderMail } from '../mailtemplates/order.model';
+import { CycicService } from '../cycic/cycic.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-order-preview',
@@ -9,13 +12,26 @@ import { stringify } from 'querystring';
   styleUrls: ['./order-preview.component.css']
 })
 export class OrderPreviewComponent implements OnInit {
-
-  constructor(private router: Router, private route: ActivatedRoute) { }
+  yourName: string;
+  yourEmail: string;
+  yourPhone: string;
+  yourAddress: string;
+  city: string;
+  zipCode: number;
+  
+  constructor(private router: Router, private route: ActivatedRoute, private http: HttpClient) { }
   lang ;
+  totalPrice = 0;
   orders : OrderMod[] =[];
   ngOnInit(): void {
     this.lang = sessionStorage.getItem('lang');
     this.orders = JSON.parse(sessionStorage.getItem('orders'));
+    this.totalPrice = 0;
+    if (sessionStorage.getItem('orders') != null) {
+      this.orders.forEach(i => {
+        this.totalPrice += i.orderPrice;
+      });
+    }
   }
   answ : string ='';
   check = function colorCheck(params:number) {
@@ -61,7 +77,84 @@ export class OrderPreviewComponent implements OnInit {
         this.answ = 'Black';
       }
     }
-
+   
     return this.answ;
   }
+  sendOrder() {
+    if(this.validation()){
+      let order = new OrderMail(
+        this.yourName,
+        this.yourEmail,
+        this.yourPhone,
+        this.yourAddress,
+        this.city,
+        this.zipCode,
+        this.orders);
+      console.log(this.lang);
+      let orderService = new CycicService(this.http);
+      orderService.createOrder(order);
+    }
+
+    // let order = new OrderMail(
+    //   this.yourName,
+    //   this.yourEmail,
+    //   this.yourPhone,
+    //   this.yourAddress,
+    //   this.city,
+    //   this.zipCode,
+    //   this.orders);
+    // console.log(this.lang);
+    // let orderService = new CycicService(this.http);
+    // orderService.createOrder(order);
+
+  }
+  validation = function analyzeValitate(){
+    if(this.yourName ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      
+      return false;
+    }
+    if(this.yourEmail ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      return false;
+    }if(this.yourPhone ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      return false;
+    }if(this.yourAddress ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      return false;
+    }if(this.city ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      return false;
+    }if(this.zipCode ==null){
+      if(this.lang == 'sr'){
+        alert("Morate popuniti sva polja.");
+      }else {
+        alert("Please enter all information.")
+      }
+      return false;
+    }
+    return true;
+  }
+
 }
